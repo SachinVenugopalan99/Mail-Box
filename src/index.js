39e5c09cb7887892acbdbca41dded4353848588a
Sachin Venugopalan <sachinvenugopalan99@gmail.com>
2023-06-08 15:48:58 +0530

    buf fixes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={'/Mail-Box'}>
     <Routes>
         <Route path='/' element={<Navigate to="/Inbox"/>} />
         <Route path='/Inbox' element={<App />} />
